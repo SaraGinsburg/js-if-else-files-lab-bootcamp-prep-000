2c069a82da4aff3d790e99d96b557bfa4b53146f
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2017-10-09 03:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan"){
+  console.log('the name is correct')
+} else {
+  console.log('expected the name to be Susan')
+}
+
 
 // Don't worry about this
 module.exports = { name, height
 }
-
